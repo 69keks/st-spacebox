0c86114f3690cefe76e585530b9d40fdf255912e
Leviathan melvilli <mehere.youthere@pm.me>
2024-09-15 12:58:59 +0530

    Slight change in fonts
    This change makes Dina font use the highest font size i.e. '10'
    This change also steps the glyphs' nerd font a little bigger so
    they can be much clearer to the eyes.
    Will be keeping this change for sometime.

--- config.def.h
+++ config.def.h
@@ -5,10 +5,10 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Dina TTF:pixelsize=12:antialias=true:autohint=true";
+static char *font = "Dina TTF:pixelsize=13:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
-	"FiraCode Nerd Font:pixelsize=12:antialias=true:autohint=true", 
+	"FiraCode Nerd Font:pixelsize=15:antialias=true:autohint=true", 
 };
 
 static int borderpx = 6;
