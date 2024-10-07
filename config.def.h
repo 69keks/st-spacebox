14897eaedc9d2fd6e820d93777886e7ed5a3f552
Leviathan melvilli <mehere.youthere@pm.me>
2024-10-07 15:11:13 +0530

    Switch to TokyoNight + Spaceduck bg

--- config.def.h
+++ config.def.h
@@ -106,23 +106,23 @@ float alpha = 0.9;
 static const char *colorname[] = {
   /* 8 normal colors */
   [0] = "#0f111b", /* background */
-  [1] = "#ea6962", /* red     */
-  [2] = "#a9b665", /* green   */
-  [3] = "#d8a657", /* yellow  */
-  [4] = "#7daea3", /* blue    */
-  [5] = "#d3869b", /* magenta */
-  [6] = "#89b482", /* cyan    */
-  [7] = "#d4be98", /* white   */
+  [1] = "#f7768e", /* red     */
+  [2] = "#9ece6a", /* green   */
+  [3] = "#e0af68", /* yellow  */
+  [4] = "#7aa2f7", /* blue    */
+  [5] = "#bb9af7", /* magenta */
+  [6] = "#7dcfff", /* cyan    */
+  [7] = "#a9b1d6", /* white   */
 
   /* 8 bright colors */
-  [8]  = "#928374", /* black   */
-  [9]  = "#ef938e", /* red     */
-  [10] = "#bbc585", /* green   */
-  [11] = "#e1bb7e", /* yellow  */
-  [12] = "#9dc2ba", /* blue    */
-  [13] = "#e1acbb", /* magenta */
-  [14] = "#a7c7a2", /* cyan    */
-  [15] = "#e2d3ba", /* white   */
+  [8]  = "#414868", /* black   */
+  [9]  = "#f7768e", /* red     */
+  [10] = "#9ece6a", /* green   */
+  [11] = "#e0af68", /* yellow  */
+  [12] = "#7aa2f7", /* blue    */
+  [13] = "#bb9af7", /* magenta */
+  [14] = "#7dcfff", /* cyan    */
+  [15] = "#c0caf5", /* white   */
 };
 
 
